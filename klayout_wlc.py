cbcb4597423e88f93425c4338735e918ad8df1e6
Jaspreet Jhoja <jaspreetjhoja@gmail.com>
2015-12-15 04:29:34 +0000

    Update klayout_wlc.py

--- klayout_wlc.py
+++ klayout_wlc.py
@@ -1,149 +1,168 @@
-# by Jasprett
+#################################################
+##File:           klayout_wlc.py
+##Author:         Jaspreet Jhoja
+##Python Interface for analysing output text data file from KLayout Software
 
-import linecache
-import os.path
+import pygame
+import pygame._view
 
-def confirm():
+load_profile = open('data.txt', "r")
+read_it = load_profile.read()
+Lines = []
+paths = []
+datapoints = []
+finaldata = []
+redundant =[]
+drawpoints = []
+
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+BLUE = (0, 0, 255)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+
+colors = [BLACK, BLUE, GREEN, RED]
+
+for line in read_it.splitlines():
+    Lines.append(line)
     
-        a = raw_input("Select a method to proceed [eg: 1 or 2] :")
-        if(a == "1"):
-            print "You selected Method 1"
-            width = input("waveguide width in microns: ")
-            run1(width)
-            confirm()
-
-        elif (a == "2"):
-            print "You selected Method 2"
-            raw_input("press enter to proceed")
-            run2()
-            confirm()
-
-        else:
-            print "invalid option, please try again"
-            print ""
-            confirm()
-
+#first new variables
+def width():
+    global paths
+    for i in Lines:
+        if "SNAME " in i:
+            a = i.replace("SNAME ","")
+            indexval = Lines.index(i)
+            indexval = indexval+7
+            wdata = Lines[indexval]
+            split= wdata.split(" ")
             
-def run1(wdth):
-    
-
-    ln = 0
-    store = []
-    Area = 0
-    nofile = 0
-    def semi(): #assorts the pasted data
-        data = raw_input("Add data here")
-        broken = data.split("\n")
-        for i in range(len(broken)):
-            b = broken[i].split("\t")
-            c = (float(b[0]),float(b[1]))
-            store.append(c)
-    semi()
-    def parea(corners): #calculates the polygon area
-        n = len(corners)
-        area = 0.0
-        for i in range(n):
-            j = (i + 1) % n
-            area += corners[i][0] * corners[j][1]
-            area -= corners[j][0] * corners[i][1]
-        area = abs(area) / 2.0
-        return area
-    Area = parea(store)
-    print "Area =",Area
-    
-    Width = wdth
-    
-    if(Width !=0):
-        Length = Area/Width
-        Length = Length/1000000
-        print "Length = ",Length
-    raw_input("press enter if you want to process another file.")
-    print ""
-
-def run2():
-    ln = 0
-    store = []
-    Area = 0
-    nofile = 0
-    start = 0
-    while True: #extract the data from the file
-            if(os.path.isfile("Data.txt")==False):
-                print "Data file is not found"
-                nofile = 1
-                break     
-            a = linecache.getline('Data.txt', ln)
-            a = str(a)
-          #  print a
-
-            if(start == 1):
-                    if(str(a[0]+a[1]+a[2]+a[3]+a[4])!="ENDEL"):
-                            a = a.replace("X","")
-                            a = a.replace("Y","")
-                            a = a.replace(" ","")
-                            a = a.replace("\n","")
-                            b = a.split(":")
-                            c = (int(b[0]),int(b[1]))
-                            store.append(c)
-                    else:
-                            break
-            if(len(a)>2):
-                if(str(a[0]+a[1]+a[2]+a[3]+a[4])=="XY 0:"):
-                    ln=ln+1
-                elif(str(a[0]+a[1])=="XY" and (int(str(a[3]+a[4]+a[5]))>0)):
-                    start = 1
-                    print a
-            ln=ln+1
-              
-    #'50.45000\t137.20000\n55.45000\t137.20000\n55.45000\t122.70000\n45.45000\t122.70000\n45.45000\t112.70000\n60.95000\t112.70000\n60.95000\t147.70000\n60.95000\t147.70000\n60.95000\t147.70000'
-    def parea(corners): #calculates the polygon area
-        n = len(corners)
-        area = 0.0
-        for i in range(n):
-            j = (i + 1) % n
-            area += corners[i][0] * corners[j][1]
-            area -= corners[j][0] * corners[i][1]
-        area = abs(area) / 2.0
-        return area
-    Area = parea(store)
-    print "Area =",Area
-    
-    Width = 0
-
-
-    if(nofile == 0):  #Calculates the waveguide width
-
-        line = 0
-        e = ""
-        Width = 0
-        trials = 0
+            for i in split:
+                if "w=" in i:
+                    fdata = i.replace("w=","")
+                    fdata = float(fdata)
+            pdata = [a,fdata]
+            paths.append(pdata)
+            
+def cdata():
+    global datapoints
+    global drawpoints
+    for j in paths:
+        foundit = 0
+        rawdata=[]
+        rawdata1 = []
+        pathname = ""
+        endpt = 'ENDEL '
+        startindex = 0
+        for i in Lines:
+            pathname = "STRNAME "+j[0] 
+            if pathname == i and foundit==0:
+                foundit = 1
+                indexval = Lines.index(pathname)
+                indexval = indexval+5
+                startpt = Lines[indexval]
+                startpt = startpt.replace("XY ","")
+                a = startpt.split(': ')
+                b = [float(a[0])/1000,float(a[1])/1000]
+                z = [float(a[0])/1500,float(a[1])/1500]
+                rawdata.append(b)
+                rawdata1.append(z)
+                startindex = indexval
         while True:
-            a = linecache.getline('Data.txt', line)
-            a = str(a)
+            startindex = startindex + 1
+            if(Lines[startindex] == endpt):
+                break
+            a = Lines[startindex]
+            a = a.split(': ')
            # print a
-            if("w="in a):
-                    #print 
-                    a = linecache.getline('Data.txt', line)
-                    b = linecache.getline('Data.txt', line+1)
-                    c = str(a+b)
-                    d = c.index("w=")
-                    while (c[d]!=" "):
-                        e = e+c[d]
-                        d=d+1
-                    
-            if("w=" in e):
-                f = e.index("w")
-                e = e[f+2:]
-                Width = float(e)
-                print "Width =",Width
-                break    
-            line=line+1
-        #Width = 0.5
-        if(Width !=0):
-            Length = Area/Width
-            Length = Length/1000000
-            print "Length = ",Length
-    raw_input("press enter if you want to process another file.")
-    print ""
+            b = [float(a[0])/1000, float(a[1])/1000]
+            z = [float(a[0])/1500,float(a[1])/1500]
+            rawdata.append(b)
+            rawdata1.append(z)
+        datapoints.append(rawdata)
+        drawpoints.append(rawdata1)
+#    print datapoints
 
+def parea(corners): #calculates the polygon area
+    n = len(corners)
+    area = 0.0
+    for i in range(n):
+        j = (i + 1) % n
+        area += corners[i][0] * corners[j][1]
+        area -= corners[j][0] * corners[i][1]
+    area = abs(area) / 2.0
+    return area
 
-confirm()
+def finalstep():
+    global finaldata
+    for i in range(len(paths)):
+        Area = parea(datapoints[i])
+        Name = paths[i][0]
+        Width = paths[i][1]
+        data = [Name, Width, Area/Width]
+        finaldata.append(data)
+width()
+cdata()
+finalstep()
+
+#Draw polygon in pygame
+def drawpoly():
+    pygame.init()
+    size = (800, 800)
+    screen = pygame.display.set_mode(size)
+    pygame.display.set_caption("waveguides lengths")
+    done = False
+    clock = pygame.time.Clock()
+    while not done:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                done = True
+        screen.fill(WHITE)
+        add = 100
+        for i in range(len(paths)):
+            a = 0
+            b = 0
+
+            for j in range(len(drawpoints[i])):
+                a = j
+                if (a == len(drawpoints[i])-1):
+                    b = a
+                else:
+                    b = a+1
+                pygame.draw.line(screen, colors[i], [(drawpoints[i][a][0]), ((drawpoints[i][a][1]))+add], [drawpoints[i][b][0], drawpoints[i][b][1]+add] , 1)
+
+            c = drawpoints[i]
+            #write data stats
+            xlimit = c[0][0]
+            ylimit = c[0][1]
+            for k in range(len(drawpoints[i])):
+                if (xlimit<= drawpoints[i][k][0]):
+                    xlimit = drawpoints[i][k][0]
+                if (ylimit<= drawpoints[i][k][1]):
+                    ylimit = drawpoints[i][k][1]
+            if (i == 0):
+                ylimit = ylimit/2 +100
+            elif(i == 1):
+                ylimit = (ylimit/2)+200
+            elif(i == 2):
+                ylimit = (ylimit/2)+300
+            elif (i == 3):
+                ylimit = (ylimit/2)+400
+            elif (i == 4):
+                ylimit = (ylimit/2)+500
+            elif (i ==5):
+                ylimit = (ylimit/2) + 600
+                
+            font = pygame.font.SysFont('Calibri', 12, True, False)
+            text1 = font.render("wg_label = "+str(finaldata[i][0]), True, BLACK)
+            screen.blit(text1, [xlimit+30, ylimit])
+            text2 = font.render("width = "+str(finaldata[i][1])+ " um", True, BLACK)
+            screen.blit(text2, [xlimit+30, ylimit+15])    
+            text3 = font.render("wg_length = "+str(finaldata[i][2])+ " um", True, BLACK)
+            screen.blit(text3, [xlimit+30, ylimit+30])    
+            add = add+100
+        pygame.display.flip()
+        clock.tick(60)
+    pygame.quit()
+
+drawpoly()
